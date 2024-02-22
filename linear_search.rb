fff76075f9a847bcfebc086eb36c677f8f215e65
thiagomartin <155474906+thiagomartin@users.noreply.github.com>
2024-02-22 12:05:57 -0300

    linear

---
+++ linear_search.rb
@@ -0,0 +1,20 @@
+def search(list, value_search)
+  for i in 0..(list.length - 1)
+    if list[i] ==  value_search
+      return i
+    end
+  end
+
+  return -1
+end
+
+my_list = [3, 1, 4, 5, 9, 2, 6, 5, 3, 5]
+value_searching = 6
+
+result = search(my_list, value_searching)
+
+if result != -1
+  puts " O valor #{value_searching} foi  na posição #{result}."
+else
+  puts "O valor #{value_searching} não foi encontrado na lista."
+end
