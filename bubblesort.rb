7af37b7e0c24350a1d56a9cdc8a667db5f3f93ae
thiagomartin <155474906+thiagomartin@users.noreply.github.com>
2024-02-22 10:29:47 -0300

    resolução problema

---
+++ bubblesort.rb
@@ -0,0 +1,14 @@
+def bubble_sort(arr)
+  n =  arr.length
+  for i in 0..n-1
+    for j in 0..n-i-2
+      if arr[j] > arr[j+1]
+      arr[j], arr[j+1] = arr[j+1], arr[j]
+      end
+    end
+  end
+end
+
+my_list = [64, 34, 25, 12, 22, 11, 90]
+bubble_sort(my_list)
+puts "lista ordenada: #{my_list}"
