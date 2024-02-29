10456331badc7e47266772bb898d6e5ee0610158
thiagomartin <155474906+thiagomartin@users.noreply.github.com>
2024-02-29 14:43:18 -0300

    finalizado mas sem dbug parou de funcionar

--- consultaData.rb
+++ consultaData.rb
@@ -4,28 +4,36 @@
 
 def parse_date(date_str)
   day, month, year = date_str.split("/").map(&:to_i)
-  [day,month, year]
+  [day, month, year]
 end
 
 def compare_dates(date1, date2)
-  return date1[2] <=> date2[2] if date1[2] != date2[2]  #[] é o elemento do array aqui é o ano
-  return date1[1] <=> date1[1] if date1[1] != date2[1] #[] aqui o mes
-  date1[0] <=> date2[0]                                 #[]dia
+  return date1[2] <=> date2[2] if date1[2] != date2[2]
+  return date1[1] <=> date2[1] if date1[1] != date2[1]
+  date1[0] <=> date2[0]
 end
 
 def binary_search(arr, target_date)
-  mid = (low +  high) / 2
-  comparison_result = compare_dates(parse_date(arr[mid]), target_date)
-  if comparison_result == 0
-    return mid
+  low, high = 0, arr.length - 1
+
+  while low <= high
+    mid = (low + high) / 2
+    comparison_result = compare_dates(parse_date(arr[mid]), target_date)
+
+    if comparison_result == 0
+      return mid
     elsif comparison_result < 0
       low = mid + 1
     else
       high = mid - 1
     end
-end
+  end
 
-return low
+  if low == arr.length
+    return -1
+  else
+    return low
+  end
 end
 
 def closest_date(arr, query_date)
@@ -39,3 +47,10 @@ def closest_date(arr, query_date)
   end
 end
 
+arr = ["22/4/1233", "1/3/633", "23/5/56645", "4/12/233"]
+queries = ["23/03/4345", "12/3/2"]
+
+queries.each do |query|
+  result = closest_date(arr, query)
+  puts result
+end
