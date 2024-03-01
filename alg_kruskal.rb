9ec106431ff89ec8b3cc4e06a29698d532450943
thiagomartin <155474906+thiagomartin@users.noreply.github.com>
2024-03-01 15:17:38 -0300

    corrigindo

--- alg_kruskal.rb
+++ alg_kruskal.rb
@@ -3,8 +3,8 @@
 
 
 class Graph
+  attr_accessor :vertices, :edges
 
-  attr_accessor :vertices,:edges
   def initialize(vertices, edges)
     @vertices = vertices
     @edges = edges
@@ -13,27 +13,27 @@ end
 
 class UnionFind
   def initialize(size)
-    @parent = Array.new(size-1)
+    @parent = Array.new(size, -1)
   end
 
-def union(x, y)
-  root_x = find(x)
-  root_y = find(y)
-  @parent[root_x] = root_y
+  def union(x, y)
+    root_x = find(x)
+    root_y = find(y)
+    @parent[root_x] = root_y
   end
 
-def find(x)
-  return x if @parent[x] == -1
-  find(@parent[x])
+  def find(x)
+    return x if @parent[x] == -1
+    find(@parent[x])
   end
 end
 
 def kruskal(graph)
-  result =  []
-  graph.edges = graph.edges.sort_by {|edge| edge[2]}
+  result = []
+  graph.edges = graph.edges.sort_by { |edge| edge[2] }
   union_find = UnionFind.new(graph.vertices)
   graph.edges.each do |edge|
-    u,v, weight = edge
+    u, v, weight = edge
 
     if union_find.find(u) != union_find.find(v)
       result << edge
@@ -45,14 +45,15 @@ end
 
 vertices = 4
 edges = [
-  [0,1,2],
-  [0,2,4],
-  [1,2,3],
-  [1,3,5],
-  [2,3,5],
+  [0, 1, 2],
+  [0, 2, 4],
+  [1, 2, 3],
+  [1, 3, 5],
+  [2, 3, 5]
 ]
 
-graph= Graph.new(vertices, edges)
+graph = Graph.new(vertices, edges)
 minimum_spanning_tree = kruskal(graph)
-puts "Arestas da árvore geradora minuma :"
-minimum_spanning_tree.each {|edge| puts "#{edge[0]} - #{edge[1]}: #{edge[2]}"}
+puts "Arestas da árvore geradora mínima:"
+minimum_spanning_tree.each { |edge| puts "#{edge[0]} - #{edge[1]}: #{edge[2]}" }
+
