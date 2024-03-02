44ab12f5802b62f4e33e208b7e5cce1ae5935bb6
thiagomartin <155474906+thiagomartin@users.noreply.github.com>
2024-03-02 18:29:45 -0300

    novo cod fatorial

--- fatorial.rb
+++ fatorial.rb
@@ -2,3 +2,20 @@ print "Digite um número para calcular o fatorial: "
 numero = gets.chomp.to_i
 fatorial = (1..numero).reduce(:*)|| 1
 puts "o fatorial de #{numero}! é: #{fatorial}"
+
+#####forma diferente poderia usar o require(de forma externa) só pra treinar 
+class Fatorial
+  def initialize(n)
+    @num = n
+  end
+def calcular_fatorial
+  if @num == 0
+    1
+  else
+    @num * Fatorial.new(@num -1).calcular_fatorial
+  end
+  end
+end
+
+n = Fatorial.new(5)
+puts "#{n.calcular_fatorial}"
